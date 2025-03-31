27fe7a6a5fc117e57da3e718fb1469370f7e1dfa
Kundan kumar dubey <dubeykundan351@gmail.com>
2025-03-31 18:54:41 +0530

    Update App.jsx

--- spotify-clone/src/App.jsx
+++ spotify-clone/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import SideBar from "./components/sidebar";
+import SideBar from "./components/SideBar";
 
 import Player from "./components/Player";
 import Display from "./components/Display";
